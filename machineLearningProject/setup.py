15852aa54118bee3c670ba43c85e5d9f1945aaef
NehaBasuthkar <48860951+NehaBasuthkar@users.noreply.github.com>
2026-04-11 14:47:22 +0530

    basic setup

---
+++ machineLearningProject/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+def get_requirements(file_path:str)->list[str]:
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if '-e.' in requirements:
+            requirements.remove('-e.')
+
+setup(
+    name='ml-project',
+    version='0.1.0',
+    author='Neha',
+    author_email='neha@example.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
