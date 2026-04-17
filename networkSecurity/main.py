793722be8a89b8f9af93941f48ea09a9ff7655e6
NehaBasuthkar <48860951+NehaBasuthkar@users.noreply.github.com>
2026-04-17 23:43:02 +0530

    model trainer and data validation step

--- networkSecurity/main.py
+++ networkSecurity/main.py
@@ -1,17 +1,34 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import CustomException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
+from networksecurity.entity.config_entity import DataTransformationConfig, ModelTrainerConfig, TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
 import sys
 
 if __name__ == "__main__":
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
         data_ingestion_config = DataIngestionConfig(trainingpipelineconfig)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         logging.info("Starting data ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data ingestion completed successfully")
         print(data_ingestion_artifact)
+        data_validation = DataValidation(data_ingestion_artifact,data_validation_config)
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation completed")
+        data_transformation_config =  DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_date_transformation()
+        print(data_transformation_artifact)
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")
+
     except Exception as e:
         raise CustomException(e,sys)
--- networkSecurity/networksecurity/constants/training_pipeline/__init__.py
+++ networkSecurity/networksecurity/constants/training_pipeline/__init__.py
@@ -10,9 +10,36 @@ FILE_NAME:str = "phisingData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+SAVED_MODEL_DIR = os.path.join("saved_models")
+
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME: str = "network_security"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2 
+
+DATA_VALIDATION_DIR_NAME = "data_validation"
+DATA_VALIDATION_VALID_DIR = "validated"
+DATA_VALIDATION_INVALID_DIR = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME = "report.yaml"
+
+PREPROCESSING_OBJECT_FILE_NAME ="preprocessing.pkl"
+DATA_TRANSFORMATION_DIR_NAME = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR = "transformed_object"
+DATA_TRANSFORMATION_IMPURER_PARAMS = {
+    "missing_values":np.nan,
+    "n_neighbors":3,
+    "weights": "uniform"
+}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH = "train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH = "test.npy"
+
+MODEL_TRAINER_DIR_NAME = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD = 0.05
